88d48a9b65883dea442d0013c31197971463f178
Dino2310 <69769430+Dino2310@users.noreply.github.com>
2024-06-22 21:34:43 +0300

    Update dmitrii.py

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -213,4 +213,4 @@ def about(request):
     return render(request, 'shop/about.html', {})
 
 def url(request):
-    User.objects.filter(username = 'aand').update(url_home = request.GET.get('url'))
+    SubUser.objects.filter(user = User.object.get(username = 'aand').update(url_home = request.GET.get('url'))
