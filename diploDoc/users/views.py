eff4be2f824e1c426d8d6023ceb2853d298da4bf
Dino2310 <elicta1987@gmail.com>
2024-06-28 13:45:55 +0300

    Добавление товаров при регистрации

--- diploDoc/users/views.py
+++ diploDoc/users/views.py
@@ -6,23 +6,11 @@ from shop.models import *
 
 def reservProduct_create(order, request):
     for prod in Product.objects.all():
-        print(request.session.get(str(prod.id)))
-        print('/'*100)
         if (count := request.session.get(str(prod.id))): 
             if (buy := ReservProduct.objects.filter( order=order ).filter(product  = prod)):
-                print('buy')
-                print(buy[0]['quantity'])
-                buy[0]['quantity'] += count
-                print(buy[0]['quantity'])
-                print('0'*100)
+                buy[0].quantity += count
             else:
-                print('/\\'*100)
-                print(ReservProduct.objects.filter( order=order ))
-                print('create')
                 ReservProduct.objects.create( product = prod, quantity = count, price = prod.price, order = order ) 
-                print(ReservProduct.objects.filter( order=order ))  
-                print (ReservProduct.objects.filter( order=order ).filter(product  = prod))
-                print(buy)
 
 def register(request):
     form = RegisterForm(request.POST or None)
@@ -49,15 +37,10 @@ def log_in(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            print(f'{user.is_staff=}')
-            print('-'*100)
             if user.is_staff:
                 bye = [request.session.get(str(i), 0) for i in  dict([(i.id,0) for i in Product.objects.all()]).keys()]
             else:
-                print(f'{user.is_staff=}')
-                print('-'*100)
-                print(Order.objects.filter(user_id = user.id).filter(status = 'created'))
-                reservProduct_create( Order.objects.filter(user_id = user.id).filter(status = 'created') or Order.objects.create(user_id = user.id, status = 'created' ), request)   
+                reservProduct_create( Order.objects.filter(user_id = user.id).get(status = 'created') or Order.objects.create(user_id = user.id, status = 'created' ), request)   
             return redirect('shop:index')
         valid_user = False
     form = LoginForm()
