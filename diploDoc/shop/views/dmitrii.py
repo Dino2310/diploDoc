58b2cc607a9e40d58cbccae28f577b0e92e869bf
Dino2310 <elicta1987@gmail.com>
2024-06-28 17:09:50 +0300

    dfsdfsdfsdfsdfs

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -228,24 +228,25 @@ def bot (request):
     url_home = "https://hagfish-star-strangely.ngrok-free.app/bot/"
     answer = json.loads(request.read())
     chat  = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= повтор сообщения"
-    if update_id != answer.get('update_id'):
-        if (res :=answer.get('message')):
-            name = res.get('from').get('first_name')
-            tg_id = res.get('from').get('id')
-            # url = SubUser.objects.filter(tg = tg_id)
-            # if url: url = url[0].url
+    requests.get(chat).json()
+    # if update_id != answer.get('update_id'):
+    #     if (res :=answer.get('message')):
+    #         name = res.get('from').get('first_name')
+    #         tg_id = res.get('from').get('id')
+    #         # url = SubUser.objects.filter(tg = tg_id)
+    #         # if url: url = url[0].url
             
-            text = res.get('text')
-            if text == '/start':
-                requests.get("https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= /start").json()
-                r = requests.post(url_home, data='/start')
-        elif (res := answer.get("callback_query")):
-            answer = res.get('chat_instance').get('date').split(',')
-            if answer[0] == 'btn': 
-                r = requests.post(url_home, data=answer[1])
+    #         text = res.get('text')
+    #         if text == '/start':
+    #             requests.get("https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= /start").json()
+    #             r = requests.post(url_home, data='/start')
+    #     elif (res := answer.get("callback_query")):
+    #         answer = res.get('chat_instance').get('date').split(',')
+    #         if answer[0] == 'btn': 
+    #             r = requests.post(url_home, data=answer[1])
         
-        # url = "https://hagfish-star-strangely.ngrok-free.app/bot/"
-        # r = requests.post(url, data=answer.get('message').get('text'))
-        update_id = answer.get('update_id')
-        return index(request)
-    else: requests.get(chat).json()
+    #     # url = "https://hagfish-star-strangely.ngrok-free.app/bot/"
+    #     # r = requests.post(url, data=answer.get('message').get('text'))
+    #     update_id = answer.get('update_id')
+    #     return index(request)
+    # else: requests.get(chat).json()
