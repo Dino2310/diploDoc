b60b05851b8c37c0ba11304f843d0bb76cfca8ee
Dino2310 <elicta1987@gmail.com>
2024-06-28 15:24:22 +0300

    12

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -230,7 +230,7 @@ def bot (request):
     
     
     url = "https://hagfish-star-strangely.ngrok-free.app/bot/"
-    r = requests.post(url, data=request)
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= пришло сообщение{r.status_code} "
-    requests.get(url).json()
+    r = requests.post(url, data=json.loads(request.read()))
+    # url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= пришло сообщение{r.status_code} "
+    # requests.get(url).json()
     return index(request)
