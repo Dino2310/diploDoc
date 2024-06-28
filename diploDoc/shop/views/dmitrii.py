e3e3adf8f5259d8831cc16732b7909449f6aa312
Dino2310 <elicta1987@gmail.com>
2024-06-28 17:53:48 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   diploDoc/shop/views/dmitrii.py

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -228,7 +228,7 @@ def bot (request):
 
     token = '7175352991:AAEsJ7VRKrzzsu6qy79kuSJkeVakLM2yrkE'
     chat_id = '900298846'
-    mess = answer.get('message')
+    mess = answer.get('message').get('chat')
 
     # if update_id != answer.get('update_id'):
         # if (res :=answer.get('message')):
