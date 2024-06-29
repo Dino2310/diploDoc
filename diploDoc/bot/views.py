fd9bbbec66d1478b45b89f939d389a783740bf25
Дмитрий Шмонов <elicta1987@gmail.com>
2024-06-29 19:12:45 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   diploDoc/bot/views.py

--- diploDoc/bot/views.py
+++ diploDoc/bot/views.py
@@ -17,6 +17,7 @@ env.read_env()
 
 @csrf_exempt
 def bot (request):
+    url_bot = 'https://api.telegram.org/bot'
     answer = json.loads(request.read())
     url_home = "https://hagfish-star-strangely.ngrok-free.app/bot/"
     
@@ -25,20 +26,27 @@ def bot (request):
 
 
     if 'message' in answer:
-        mess_text_date = answer.get('message').get('chat').get('first_name') # Тут выцепляются данные пользователя отправившего сообщение
+        name = answer.get('message').get('chat').get('first_name') # Тут выцепляются данные пользователя отправившего сообщение
         chat_id = answer.get('message').get('chat').get('id')
         mess_text = answer.get('message').get('text')  # Это текст самого сообщения
-        mess = str(mess_text) +','+str(mess_text_date) + ','+ str(chat_id)
+        mess = str(mess_text) +','+str(name) + ','+ str(chat_id)
 
     else:  
         mess_callb_data = answer.get("callback_query").get('data')
         chat_id = answer.get('callback_query').get('from').get('id')
-        mess = str(mess_callb_data)+','+ str(chat_id)
+        name =  answer.get('callback_query').get('from').get('first_name')
+        mess = str(mess_callb_data)+','+str(name)+','+ str(chat_id)
 
-    if mess.startswith('/start'):
-        r = requests.post(url_home, data=mess)
+    if mess_text == '/start':
+        reply_markup = {'inline_keyboard': [
+                            [{'text': 'Перейти за покупками', 'url': 't.me/ghghguihtuh_bot/elicta'},
+                            {'text': 'Отправиться домой', 'callback_data': 'home'}]]}
+        data = {'chat_id': chat_id, 'text': 'Привет, '+name , 'reply_markup': json.dumps(reply_markup)}
+        requests.post(f'{url_bot}{token}/sendMessage', data=data)
 
     elif mess.startswith('btn'):
+        r = requests.post(url_home, data=mess)
+    elif mess.startswith('home'):
         r = requests.post(url_home, data=mess)
     if r.status_code != 200:
         mess = f"извините, но в данный момент Ваше устройсвто отключено или не имеет доступа в интрнет"
