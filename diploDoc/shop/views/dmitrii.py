2fa682323cc4185ab33b55f40e112016c7c2a3ed
Дмитрий Шмонов <elicta1987@gmail.com>
2024-06-29 15:43:09 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   diploDoc/shop/views/dmitrii.py

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -238,7 +238,7 @@ def bot (request):
     else:  
         mess_callb_data = answer.get("callback_query").get('data')
         chat_id = answer.get('callback_query').get('from').get('id')
-        mess = str(mess_callb_data) #+','+ chat_id
+        mess = str(mess_callb_data)+','+ str(chat_id)
 
     if mess.startswith('/start'):
         r = requests.post(url_home, data=mess)
