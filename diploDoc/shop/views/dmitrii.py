079cd2cab97acbf24a15c156ccfb8daf1d0afd42
Дмитрий Шмонов <elicta1987@gmail.com>
2024-06-29 13:39:54 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   diploDoc/shop/views/dmitrii.py

--- diploDoc/shop/views/dmitrii.py
+++ diploDoc/shop/views/dmitrii.py
@@ -244,34 +244,6 @@ def bot (request):
     elif mess.startswith('btn'):
         requests.post(url_home, data=mess)
     
-
-    # if update_id != answer.get('update_id'):
-        # if (res :=answer.get('message')):
-        #     name = res.get('from').get('first_name')
-        #     tg_id = res.get('from').get('id')
-        #     # url = SubUser.objects.filter(tg = tg_id)
-        #     # if url: url = url[0].url
-            
-        #     text = res.get('text')
-        #     if text == '/start':
-        #         requests.get("https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= /start").json()
-        #         r = requests.post(url_home, data='/start')
-        # elif (res := answer.get("callback_query")):
-        #     answer = res.get('chat_instance').get('date').split(',')
-        #     if answer[0] == 'btn': 
-        #         r = requests.post(url_home, data=answer[1])
-        
-        # # url = "https://hagfish-star-strangely.ngrok-free.app/bot/"
-        # # r = requests.post(url, data=answer.get('message').get('text'))
-        # else:
-        #     mess = answer
-        #     requests.get(chat).json()
-
-    # update_id = answer.get('update_id')
-            
-    # else: 
-    #     mess = 'повторное сообщение'
-    #     requests.get(chat).json()
-    chat  = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= {mess}"
-    requests.get(chat)
+    # chat  = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text= {mess}"
+    # requests.get(chat)
     return index(request)
