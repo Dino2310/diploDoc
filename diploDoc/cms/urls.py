d00efdb0e3771aa2d461c7747ca35de8db7359a7
Klepa77 <gonza367@mail.ru>
2024-07-01 19:05:19 +0300

    сделал задачи

--- diploDoc/cms/urls.py
+++ diploDoc/cms/urls.py
@@ -10,5 +10,12 @@ urlpatterns = [
     path('customers/',views.customers, name='customers'),
     path('add_product/',views.add_product, name='add_product'),
     path('edit_product/<int:pk>',views.edit_product,name='edit_product'),
-    path('archive_product/<int:pk>',views.archive_product, name='archive_product')
+    path('archive_product/<int:pk>',views.archive_product, name='archive_product'),
+    path('order_detail/<int:pk>',views.order_detail, name='order_detail'),
+    path('customer_detail/<int:pk>',views.customer_detail, name='customer_detail'),
+    path('advertisements',views.advertisements, name='advertisements'),
+    path('advertisement_create/',views.advertisement_create, name='advertisement_create'),
+    path('advertisement_delete/<int:pk>',views.advertisement_delete, name='advertisement_delete'),
+    path('advertisement_update/<int:pk>',views.advertisement_update, name='advertisement_update'),
+
 ]
